02fec522f3e40e6c23e21cad93a454401af10e86
jackarian <ariannagiacomo@gmail.com>
2021-07-24 19:16:10 +0200

    Aggiornamento Git ignore

--- jackdomonic/urls.py
+++ jackdomonic/urls.py
@@ -2,6 +2,7 @@
 from django.urls import include, path
 from control.admin import admin_site
 
+
 urlpatterns = [
     path('control/', include('control.urls')),
     path('myadmin/', admin_site.urls),
